1cc0a0515dd1e845813319029b1386be6ef32967
kalalnanigoud <42138449+kalalnanigoud@users.noreply.github.com>
2025-10-28 06:29:47 -0400

    Create App component with BankTable integration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BankTable from "./components/BankTable";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", margin: "40px" }}>
+      <h2>Bank Accounts</h2>
+      <BankTable />
+    </div>
+  );
+}
+
+export default App;
