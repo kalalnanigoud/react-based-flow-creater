c1c21d260c8a5129b6b72a81f4f98e565a0eb3ec
kalalnanigoud <42138449+kalalnanigoud@users.noreply.github.com>
2025-10-28 06:45:43 -0400

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h3>Nodes</h3>
+      <ul>
+        <li>Start</li>
+        <li>API Call</li>
+        <li>Condition</li>
+        <li>End</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
